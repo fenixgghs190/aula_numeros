5ffe5ff88d64c72b13a66bdb62a2184ae6d9c192
Felipe <felipepdoliveira@gmail.com>
2026-03-05 10:52:24 -0300

    primeiro commit aula numeros

---
+++ numeros.py
@@ -0,0 +1,32 @@
+print('Números inteiros')
+print()
+valor_a = 10
+valor_b = 5
+
+print('Soma: ', valor_a, '+', valor_b, '=', valor_a + valor_b)
+print('Subtrair: ', valor_a, '-', valor_b, '=', valor_a - valor_b)
+print('Multiplicar: ', valor_a, '*', valor_b, '=', valor_a * valor_b)
+print('Dividir: ', valor_a, '/', valor_b, '=', valor_a / valor_b)
+print('Potencia: ', valor_a, '**', valor_b, '=', valor_a ** valor_b)
+print()
+
+print('Números ponto flutuante')
+print()
+
+valor_a = 10.0
+valor_b = 5.0
+
+print('Soma: ', valor_a, '+', valor_b, '=', valor_a + valor_b)
+print('Subtrair: ', valor_a, '-', valor_b, '=', valor_a - valor_b)
+print('Multiplicar: ', valor_a, '*', valor_b, '=', valor_a * valor_b)
+print('Dividir: ', valor_a, '/', valor_b, '=', valor_a / valor_b)
+print('Potencia: ', valor_a, '**', valor_b, '=', valor_a ** valor_b)
+print()
+
+print('Divisão inteira')
+print()
+
+valor_a = 7
+valor_b = 2
+print('valor_a // valor_b', valor_a, valor_b, 7 // 2)
+print('valor_a % valor_b', valor_a, valor_b, 7 % 2)
