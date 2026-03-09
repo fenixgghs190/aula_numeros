761ae5bf702812db5408eebfbe5120ae587e004a
fenixgghs190 <felipepdoliveira@gmail.com>
2026-03-09 09:51:02 -0300

    finalização imc

--- imc.py
+++ imc.py
@@ -7,13 +7,13 @@ ano = int(input('Ano de nascimento: '))
 Cálculo IMC
 '''
 
-imc = peso / (altura * altura)
+imc = int(peso / (altura * altura))
 
 '''
 Verificar se o ano é bisexto e idade é múltiplo de 7
 '''
 
-print('Seu imc é {}'.format(imc))
+print('Seu imc é: {}'.format(imc))
 print('Idade dividido por 7: {}'.format(idade/7))
 print('Ano dividido por 4: {}'.format(ano/4))
 
