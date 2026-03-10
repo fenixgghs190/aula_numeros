1a82919414b8c59df39d5b665dbfae599d685c64
fenixgghs190 <felipepdoliveira@gmail.com>
2026-03-10 08:04:43 -0300

    commit matematica

---
+++ teste.py
@@ -0,0 +1,4 @@
+n = float(input('Entre com um número maior que zero: '))
+expoente = float(input('Entre com o expoente: '))
+
+print('o valor de {} elevado a {} é {}'.format(n, expoente, n**expoente))
